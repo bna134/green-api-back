d1419d98d786021e21691cf6dc4efa9a90ce7419
unknown <margarita.veselova@marketguru.io>
2026-04-19 09:08:55 +0300

    ready

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import { routes } from './routes/router'
+import cors from 'cors';
 
 dotenv.config();
 
 export const app: Application = express();
 const port = process.env.PORT || 9000;
 
+app.use(cors()); 
 app.use(express.json()); 
 app.use('/', routes);
 app.set('trust proxy', 1); 
