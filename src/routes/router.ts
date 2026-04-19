189827e9f6e80daac20be33fed75c933849950e4
unknown <margarita.veselova@marketguru.io>
2026-04-19 20:47:46 +0300

    final

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { accountRouter } from "./accounts.router";
 import { sendingRouter } from "./sending.router";
+import cors from  'cors';
 
 export const routes = Router();
+routes.use(cors({
+  origin: 'https://bna134.github.io',
+  methods: ['GET', 'POST']
+}))
 routes.use('/account', accountRouter);
 routes.use('/sending', sendingRouter);
