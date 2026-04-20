5670ddbab37071944d46aa735ca7c822b6d21533
unknown <margarita.veselova@marketguru.io>
2026-04-20 05:53:31 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ app.use(function (req, res, next) {
     next();
 })
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json()); 
 app.use('/', routes);
 app.set('trust proxy', 1); 
