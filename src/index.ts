8804e1c9f65bbaa0e7c823a9ef0281607464eb51
unknown <margarita.veselova@marketguru.io>
2026-04-20 06:16:38 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
+app.use(express.text({type: "text/plain"}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json()); 
